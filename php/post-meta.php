921e99e6b4cf7a31be28073014e254e557a18bea
Josh Robbs <josh@joshrobbs.com>
2022-06-05 21:05:26 -0400

    Updated post meta

--- php/post-meta.php
+++ php/post-meta.php
@@ -14,15 +14,15 @@ function jwr_post_meta_fn($atts = array(), $content = null){
 	$author_id = get_the_author_meta('ID');
 	$author_link = get_author_posts_url($author_id);
 
-	$post_date = get_the_date();
+	$post_date = get_the_date('F j, Y');
 
 	$category = get_the_term_list($id,'category', "Filed under: ",', ');
 	$my_tags = get_the_term_list($id,'post_tag', "Topics: ",', ');
 	
-		$difficulty = get_the_term_list($id,'difficulties', "Difficulty: ",', ');
-		$required_plugins = get_field('required_software');
-		$code_topics = get_the_term_list($id,'code-topic', "Topics: ",', ');
-		$functions = get_the_term_list($id,'function', "Difficulty: ",', ');
+	$difficulty = get_the_term_list($id,'difficulties', "Difficulty: ",', ');
+	$required_plugins = get_field('required_software');
+	$code_topics = get_the_term_list($id,'code-topic', "Topics: ",', ');
+	$functions = get_the_term_list($id,'function', "Difficulty: ",', ');
 		// these were in an if statement. do i actually need it? Do I save that much time?
 
 
@@ -35,10 +35,13 @@ function jwr_post_meta_fn($atts = array(), $content = null){
 	</style>
 	<?php
 	echo "<div class='custom-meta'>";
-	echo "This $post_type was<br />";
-	echo "published by <a href='$author_link'>$author</a><br />";
-	echo "on $post_date<br />";
-	$mod_date = get_the_modified_date( );
+	echo "This $post_type was";
+	echo " published by <a href='$author_link'>$author</a>";
+	echo " on $post_date";
+	$mod_date = get_the_modified_date('F j, Y');
+	if($mod_date != $post_date) {
+		echo " (last udpate: $mod_date)";
+	}
 	//? add last updated?
 	if($category) {
 		echo "$category";
