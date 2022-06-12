18011c6d26466b7570474b108335bdeac1d0c147
Josh Robbs <josh@joshrobbs.com>
2022-06-12 11:06:10 -0400

    Update HR margin

--- php/post-meta.php
+++ php/post-meta.php
@@ -21,6 +21,7 @@ function jwr_post_meta_fn($atts = array(), $content = null){
 	
 	$difficulty = get_the_term_list($id,'difficulties', "Difficulty: ",', ');
 	$required_plugins = get_field('required_software');
+	$related_code_snippets = get_field('related_code_snippets');
 	$code_topics = get_the_term_list($id,'code-topic', "Topics: ",', ');
 		// these were in an if statement. do i actually need it? Do I save that much time?
 
@@ -30,6 +31,9 @@ function jwr_post_meta_fn($atts = array(), $content = null){
 	<style>
 		.custom-meta {
 			text-align: right;
+		}
+		.custom-meta hr {
+			margin: 1rem 0;
 		}
 	</style>
 	<?php
@@ -49,7 +53,8 @@ function jwr_post_meta_fn($atts = array(), $content = null){
 		echo "<br />$my_tags";
 	}
 	if($difficulty) {
-		echo "<br />$difficulty<br />";
+		echo "<hr>";
+		echo "$difficulty";
 	}
 	if($required_plugins) {
 		echo "<hr>";
@@ -60,6 +65,16 @@ function jwr_post_meta_fn($atts = array(), $content = null){
 		}
 		$plugin_list = implode(", ",$plugin_list_array);
 		echo $plugin_list;
+	}
+	if($related_code_snippets) {
+		echo "<hr>";
+		echo "Related code snippets:<br />";
+		foreach($related_code_snippets as $snippet){
+			$link = get_permalink($snippet->ID);
+			$snippet_list_array[] = "<a href='$link'>$snippet->post_title</a>";
+		}
+		$snippet_list = implode(", ",$snippet_list_array);
+		echo $snippet_list;
 	}
 	
 	if($code_topics) {
