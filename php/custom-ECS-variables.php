20865e956831aed1c8fab6fae4c62370e9b480ca
Josh Robbs <josh@joshrobbs.com>
2022-06-25 14:06:01 -0400

    Add initial set up

--- php/custom-ECS-variables.php
+++ php/custom-ECS-variables.php
@@ -3,8 +3,12 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 add_filter( 'ecs_vars', function ( $custom_vars) {
 
+	// Format
+	/*
+	$custom_vars['VARIABLE TO ADD'] = OUTPUT;
+	*/
 
-
+	//? can this be dynamic? Does it know the page data?
 
 
 
