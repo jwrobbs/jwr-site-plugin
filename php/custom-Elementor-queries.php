e6d6126887a664b1b777b4825b23a604b96f8dff
Josh Robbs <josh@joshrobbs.com>
2022-06-25 14:18:37 -0400

    Add all_posts query

--- php/custom-Elementor-queries.php
+++ php/custom-Elementor-queries.php
@@ -7,4 +7,18 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 add_action( 'elementor/query/<the query ID>', function( $query ) {
     $query->set( 'post_type', [ 'custom-post-type1', 'custom-post-type2' ] ); // the query modifications you want to make
 } );
+
+/*
+	# list of query vars
+	https://codex.wordpress.org/WordPress_Query_Vars
+	https://developer.wordpress.org/reference/classes/wp_query/
+
+	This solution found originally at https://www.scratchcode.io/add-multiple-post-types-in-posts-widget-in-elementor/
 */
+
+*/
+add_action( 'elementor/query/all_posts', function( $query ) {
+    $query->set( 'post_type', [ 'post', 'review', 'tutorial', 'review','code-snippet' ] ); 
+	$query->set('post_status ','publish');
+} );
+
