a269aaf3e63bf72ab832dd3b81c1f6edcf23b1f5
Josh Robbs <josh@joshrobbs.com>
2022-06-26 09:49:08 -0400

    Add home page post type links

--- php/shortcode.php
+++ php/shortcode.php
@@ -13,3 +13,36 @@ function jwr_code_fn($atts = array(), $content = null){
 }
     
 add_shortcode( 'jwr-code' , 'jwr_code_fn' );
+
+
+function jwr_home_cpt_links_fn($atts = array(), $content = null){
+    ob_start();
+
+    // start output
+    $cpt_array = array(
+        'post',
+        'tutorial',
+        'review',
+        'code-snippet',
+    );
+    foreach( $cpt_array as $cpt ){
+        // get proper name and url
+        $cpt_obj = get_post_type_object($cpt);
+        
+        //output
+        $label = $cpt_obj->label;
+        if( $label ){
+            echo "<a href='$cpt'>$label</a><br>";
+        }
+    }
+
+    //return output
+    wp_reset_postdata();
+    $thisOutput = ob_get_clean();
+    if( $thisOutput ){
+        $thisOutput = "<div class='home-pt-link-container'>$thisOutput</div>";
+    }
+    return $thisOutput;
+}
+    
+add_shortcode( 'jwr_home_cpt_links' , 'jwr_home_cpt_links_fn' );
