fa1490d2d47e873ac08c4561204da7e319243ce2
Josh Robbs <josh@joshrobbs.com>
2022-07-02 10:28:03 -0400

    Add byline for home

--- php/custom-ECS-variables.php
+++ php/custom-ECS-variables.php
@@ -56,6 +56,55 @@ add_filter( 'ecs_vars', function ( $custom_vars ) {
 	}
 
 
+	$id = get_the_ID();
+	$this_post = get_post( $id );
+	if( $this_post && $post_type ){
+		
+		switch ($post_type) {
+			case 'tutorial':
+				$difficulties = get_the_terms($id,'difficulties');
+				$difficulty = $difficulties[0]->name;
+				if( $difficulty == "Apprentice"){
+					$article = "an";
+				}else {
+					$article = "a";
+				}
+				$output = "$difficulty tutorial";
+				break;
+			case 'review':
+				$review_categories = get_the_terms( $id, 'review-category' );
+				$review_category = strtolower($review_categories[0]->name);
+				$output = "$review_category review";
+				break;
+			 case 'code-snippet':
+				$output = "code snippet";
+				break;
+			case 'post':
+				$output = "article";
+				break;
+			default:
+				$output = "$post_type";
+		}
+		$output = ucfirst( $output );
+
+		$date = get_the_date( "M d, Y", $id );
+		
+		if( $date ){
+			$output = "$output posted on $date";
+		}
+
+		$author = get_the_author_meta( 'nickname');
+		if( $author ){
+			$output = "$output by $author";
+		}
+
+
+		$custom_vars['jwr_archive_byline'] = "$output";
+	}
+
+
+
+
 
 
 	
