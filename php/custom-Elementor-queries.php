ef9d9fb7861d001d7e60f12ad0d11c2d0953b334
Josh Robbs <josh@joshrobbs.com>
2022-07-02 08:46:31 -0400

    Add comments

--- php/custom-Elementor-queries.php
+++ php/custom-Elementor-queries.php
@@ -1,23 +1,25 @@
 <?php
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-// format
-
-/*
-add_action( 'elementor/query/<the query ID>', function( $query ) {
+/**
+ * Creates custom queries for use in Elementor
+ * 
+ * Format:
+ * 
+	add_action( 'elementor/query/<the query ID>', function( $query ) {
     $query->set( 'post_type', [ 'custom-post-type1', 'custom-post-type2' ] ); // the query modifications you want to make
-} );
-*/
+	} );
 
-/*
-	# list of query vars
-	https://codex.wordpress.org/WordPress_Query_Vars
-	https://developer.wordpress.org/reference/classes/wp_query/
+ * 
+ * # list of query vars
+ * https://codex.wordpress.org/WordPress_Query_Vars
+ * https://developer.wordpress.org/reference/classes/wp_query/
+ * 
+ * This solution found originally at https://www.scratchcode.io/add-multiple-post-types-in-posts-widget-in-elementor/ 
+ */
 
-	This solution found originally at https://www.scratchcode.io/add-multiple-post-types-in-posts-widget-in-elementor/
-*/
 
-add_action( 'elementor/query/all_posts', function( $query ) {
+add_action( 'elementor/query/all_posts', function( $query ) { // query ID = all_posts
     $query->set( 'post_type', [ 'post', 'review', 'tutorial', 'review','code-snippet' ] ); 
 	$query->set('post_status ','publish');
 });
