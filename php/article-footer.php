c4ab9e7382bcf0aa8e2e2696a3bc761ea04b5d36
Josh Robbs <josh@joshrobbs.com>
2022-07-09 09:08:15 -0400

    Add post_tag to article footer

--- php/article-footer.php
+++ php/article-footer.php
@@ -11,19 +11,13 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 function jwr_article_footer_fn($atts = array(), $content = null){
     ob_start();
     // start output ----------
-
+	$id = get_the_ID();
 	$post_type = get_post_type(  );
-
-	
 	
 	if( 'post' == $post_type){ // return on post
-		ob_end_clean();
-		wp_reset_postdata();
-		return;
+		// nothing yet
 	}elseif( 'tutorial' == $post_type){ // tutorial
-		ob_end_clean();
-		wp_reset_postdata();
-		return;
+		// nothing yet
 	}elseif( 'review' == $post_type){ // review
 		echo do_shortcode( '[jwr-review-footer]' );
 	}elseif( 'snippet' == $post_type){ // code
@@ -33,8 +27,13 @@ function jwr_article_footer_fn($atts = array(), $content = null){
 		echo "No post type match for '$post_type'";
 	}
 
-
-
+	// check for post_tag
+	$my_tags = get_the_term_list($id,'post_tag', "Topics: ",', ');
+	//display
+	
+	if($my_tags) {
+		echo "<div class='jwr-tags'>$my_tags</div>";
+	}
 
     //return output ----------
     wp_reset_postdata();
